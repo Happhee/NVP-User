aa7544c94b30dfb8273a440d3d1fd8ae501d2e87
seohee0112 <ghdtj7@gmail.com>
2021-12-22 11:33:39 +0900

    [ADD] AuthStack 스크린 기본 유아이 추가

---
+++ src/screens/AuthStack/Login.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Login from '../../containers/auth/Login';
+import { View, Text, Button } from 'react-native';
+
+function LoginScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Login />
+        </View>
+    )
+}
+
+export default LoginScreen;
