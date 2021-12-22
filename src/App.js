956f8fe4bcc239c146b626b64e3da2e8d6aad8a6
seohee0112 <ghdtj7@gmail.com>
2021-12-22 11:57:41 +0900

    [ADD] 앱에 모든 스택 연결하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AuthStackScreen from './navigations/AuthStackScreen';
+import MainTabScreen from './navigations/MainTab';
+import { FLOWPREDICATE_TYPES } from '@babel/types';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 
@@ -16,9 +19,16 @@ const App = () => {
   }, []);
 
   return (
-    <View style={{ backgroundColor: "white" }}>
-      <Text>테스트</Text>
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Auth">
+          <Stack.Screen name="Auth" component={AuthStackScreen}
+            options={{ headerShwon: false }}
+          />
+          <Stack.Screen name="Main" component={MainTabScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 export default App;
