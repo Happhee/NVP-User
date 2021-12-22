cd8de26a09ac6f6f9f60ea2b59ed9e80e092165f
seohee0112 <ghdtj7@gmail.com>
2021-12-22 23:02:46 +0900

    [UPGRADE] 앱 업그레이드

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Auth">
+        <Stack.Navigator initialRouteName="Main">
           <Stack.Screen name="Auth" component={AuthStackScreen}
             options={{ headerShown: false }}
           />
