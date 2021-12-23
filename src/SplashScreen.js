001473c0a733de94c0ec10b04d3c22356d4e232b
seohee0112 <ghdtj7@gmail.com>
2021-12-23 20:36:57 +0900

    [ADD] 스플래쉬 스크린 추가

--- src/SplashScreen.js
+++ src/SplashScreen.js
@@ -2,33 +2,33 @@ import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { View, Text } from 'react-native';
 import { useDispatch } from 'react-redux';
-import
-    function SplashScreen({ navigation, props }) {
-        const dispatch = useDispatch();
+
+function SplashScreen({ navigation, props }) {
+    const dispatch = useDispatch();
 
 
-        useEffect(() => {
-            setTimeout(() => {
-                AsyncStorage.getItem('userUniqueId')
-                    .then((value) => {
-                        if (value != null) {
+    useEffect(() => {
+        setTimeout(() => {
+            AsyncStorage.getItem('userUniqueId')
+                .then((value) => {
+                    if (value != null) {
 
-                        } else {
-                            navigation.replace('Auth');
-                            console.log(value);
+                    } else {
+                        navigation.replace('Auth');
+                        console.log(value);
 
-                        }
                     }
-                    );
+                }
+                );
 
-            }, 3000);
-        }, []);
+        }, 3000);
+    }, []);
 
-        return (
-            <View >
-                <Text>스플래쉬 화면</Text>
-            </View>
-        )
-    }
+    return (
+        <View >
+            <Text>스플래쉬 화면</Text>
+        </View>
+    )
+}
 
 export default SplashScreen;
