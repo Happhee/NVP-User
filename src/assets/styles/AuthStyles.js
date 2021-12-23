ceb224361330db44bb4aa27d7d16d510ac2ba14c
seohee0112 <ghdtj7@gmail.com>
2021-12-23 12:52:28 +0900

    [ADD] authStyle 배치도 추가

---
+++ src/assets/styles/AuthStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+import colors from "../colors/color";
+const authStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        width: '100%',
+    },
+    header: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    },
+    headerFont: {
+        color: colors.nvpRoot,
+        fontFamily: 'DoHyeon-Regular',
+        fontSize: 60,
+        textDecorationLine: 'underline'
+    },
+    title: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titleFont: {
+        color: colors.nvpRoot,
+        fontFamily: 'DoHyeon-Regular',
+        fontSize: 30,
+    },
+    content: {
+        flex: 3,
+        backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: 'yellow',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default authStyles;
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,35 +4,49 @@ import { View, Text, TextInput } from 'react-native';
 import { Button } from 'react-native-paper'
 import UserButton from '../UserButton';
 import colors from '../../assets/colors/color';
+import authStyles from '../../assets/styles/authStyles';
+
 function Login(props) {
     let [passWord, setPassword] = useState('');
     const uniqueId = deviceInfoModule.getUniqueId();
     console.log(props)
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ color: colors.nvpRoot, fontFamily: 'DoHyeon-Regular', fontSize: 40, textDecorationLine: 'underline' }} >sasd</Text>
+        <View style={authStyles.container}>
+            <View style={authStyles.header}>
+                <Text style={authStyles.headerFont}>NVP</Text>
 
-            <TextInput placeholder="PW"
-                onChangeText={inputPw => setPassword(inputPw)}
-            />
-            <UserButton
-                buttonName="LOGIN"
-                onPress={function () {
-                    const dataToSubmit = {
-                        uniqueId: uniqueId,
-                        passWord: passWord
-                    }
-                    props.onPressLogin(dataToSubmit);
-                    setPassword('');
-                    props.navigation.navigate('Main')
-                }} />
+            </View>
+            <View style={authStyles.title}>
+                <Text style={authStyles.titleFont}>등록한 간편 비밀번호를</Text>
+                <Text style={authStyles.titleFont}>입력해주세요</Text>
 
-            <UserButton
-                buttonName="회원가입"
-                onPress={function () {
-                    props.navigation.navigate('CheckCertificate')
-                }} />
+            </View>
+            <View style={authStyles.content}>
+                <TextInput placeholder="PW"
+                    onChangeText={inputPw => setPassword(inputPw)}
+                />
+
+            </View>
+            <View style={authStyles.footer}>
+                <UserButton
+                    buttonName="LOGIN"
+                    onPress={function () {
+                        const dataToSubmit = {
+                            uniqueId: uniqueId,
+                            passWord: passWord
+                        }
+                        props.onPressLogin(dataToSubmit);
+                        setPassword('');
+                        props.navigation.navigate('Main')
+                    }} />
+
+                <UserButton
+                    buttonName="회원가입"
+                    onPress={function () {
+                        props.navigation.navigate('CheckCertificate')
+                    }} />
+            </View>
         </View >
     )
 }
