23eaaa9798c12663089c814f72b63f0ba1c0f156
seohee0112 <ghdtj7@gmail.com>
2021-12-23 14:13:16 +0900

    [ADD] 키보드 내리는 기능 추가

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,68 +1,81 @@
 import React, { useState } from 'react';
 import deviceInfoModule from 'react-native-device-info';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Keyboard } from 'react-native';
 import { Button } from 'react-native-paper'
 import UserButton from '../UserButton';
 import colors from '../../assets/colors/color';
 import authStyles from '../../assets/styles/authStyles';
-
+import { TouchableWithoutFeedback } from 'react-native';
 function Login(props) {
     let [passWord, setPassword] = useState('');
     const uniqueId = deviceInfoModule.getUniqueId();
     console.log(props)
 
     return (
-        <View style={authStyles.container}>
-            <View style={authStyles.header}>
-                <Text style={authStyles.headerFont}>NVP</Text>
-            </View>
-            <View style={authStyles.title}>
-                <Text style={authStyles.titleFont}>등록한 간편 비밀번호를</Text>
-                <Text style={authStyles.titleFont}>입력해주세요</Text>
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+            console.log('dismissed keyboard'); //console 확인용
+        }}>
+            <View style={authStyles.container}>
 
-            </View>
-            <View style={authStyles.content}>
-                {/* <View style={authStyles.insideContent}> */}
-                <TextInput
-                    style={authStyles.contentInput}
-                    placeholder="PassWord"
-                    numberOfLines={1}
-                    secureTextEntry
-                    onChangeText={inputPw => setPassword(inputPw)}
-                />
-                <View style={authStyles.contentButton}>
+                <View style={authStyles.header}>
+                    <Text style={authStyles.headerFont}>NVP</Text>
+                </View>
+                <View style={authStyles.title}>
+                    <Text style={authStyles.titleFont}>등록한 간편 비밀번호를</Text>
+                    <Text style={authStyles.titleFont}>입력해주세요</Text>
+
+                </View>
+
+                <View style={authStyles.content} >
+
+                    <TextInput
+                        style={authStyles.contentInput}
+                        placeholder="PassWord"
+                        numberOfLines={1}
+                        secureTextEntry
+                        onChangeText={inputPw => setPassword(inputPw)}
+                    />
+
+
+                    <View style={authStyles.contentButton}>
+                        <UserButton
+
+                            buttonName="로그인"
+                            onPress={function () {
+                                const dataToSubmit = {
+                                    uniqueId: uniqueId,
+                                    passWord: passWord
+                                }
+                                props.onPressLogin(dataToSubmit);
+                                setPassword('');
+                                // props.navigation.navigate('Main')
+                                Keyboard.dismiss();
+                            }} />
+
+                        <UserButton
+                            buttonName="회원가입"
+                            onPress={function () {
+                                props.navigation.navigate('CheckCertificate')
+                            }} />
+                    </View>
+
+
+                </View>
+
+                <View style={authStyles.footer}>
                     <UserButton
-
-                        buttonName="로그인"
-                        onPress={function () {
-                            const dataToSubmit = {
-                                uniqueId: uniqueId,
-                                passWord: passWord
-                            }
-                            props.onPressLogin(dataToSubmit);
-                            setPassword('');
-                            props.navigation.navigate('Main')
-                        }} />
-
-                    <UserButton
-                        buttonName="회원가입"
+                        buttonName="간편번호를 잊어버리셨나요?"
                         onPress={function () {
                             props.navigation.navigate('CheckCertificate')
                         }} />
+
+
                 </View>
 
+            </View >
+        </TouchableWithoutFeedback>
 
-                {/* </View> */}
-
-            </View>
-            <View style={authStyles.footer}>
-                <UserButton
-                    buttonName="간편번호를 잊어버리셨나요?"
-                    onPress={function () {
-                        props.navigation.navigate('CheckCertificate')
-                    }} />
-            </View>
-        </View >
     )
 }
 
