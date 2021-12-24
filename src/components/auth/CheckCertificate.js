2f7ec5036e4634cf6d4420b175f112539c149ff5
seohee0112 <ghdtj7@gmail.com>
2021-12-24 14:46:22 +0900

    [ADD] 카메라 켜는 곳 뷰 추가

--- src/components/auth/CheckCertificate.js
+++ src/components/auth/CheckCertificate.js
@@ -4,7 +4,6 @@ import { View, Text, TextInput, Keyboard } from 'react-native';
 import RegisterButton from '../RegisterButton';
 import NextButton from '../NextButton';
 import signUp from '../../assets/styles/signUp';
-import { TouchableWithoutFeedback } from 'react-native';
 
 function CheckCertificate(props) {
     let [passWord, setPassword] = useState('');
@@ -13,34 +12,33 @@ function CheckCertificate(props) {
     const uniqueId = deviceInfoModule.getUniqueId();
 
     return (
-        <TouchableWithoutFeedback onPress={() => {
-            Keyboard.dismiss();
-            console.log('dismissed keyboard'); //console 확인용
-        }}>
-            <View style={signUp.container}>
 
-                <View style={signUp.header}>
-                    <Text style={signUp.headerFont}>NVP</Text>
-                </View>
-                <View style={signUp.title}>
-                    <Text style={signUp.titleFont}>신분증/백신증명서</Text>
-                    <Text style={signUp.titleFont}>등록</Text>
-                </View>
+        <View style={signUp.container}>
 
-                <View style={signUp.content} >
-                    <RegisterButton buttonName="신분증 등록" />
-                    <RegisterButton buttonName="백신 증명서 등록" />
-                </View>
+            <View style={signUp.header}>
+                <Text style={signUp.headerFont}>NVP</Text>
+            </View>
+            <View style={signUp.title}>
+                <Text style={signUp.titleFont}>신분증/백신증명서</Text>
+                <Text style={signUp.titleFont}>등록</Text>
+            </View>
 
-                <View style={signUp.footer}>
-                    <NextButton
-                        onPress={function () {
-                            props.navigation.navigate('SetPassword')
-                        }} />
-                </View>
+            <View style={signUp.content} >
+                <RegisterButton buttonName="신분증 등록"
+                    onPress={function () {
+                        props.navigation.navigate('TakeIdCard')
+                    }} />
+                <RegisterButton buttonName="백신 증명서 등록" />
+            </View>
 
-            </View >
-        </TouchableWithoutFeedback >
+            <View style={signUp.footer}>
+                <NextButton
+                    onPress={function () {
+                        // props.navigation.navigate('SetPassword')
+                    }} />
+            </View>
+
+        </View >
 
     )
 }
