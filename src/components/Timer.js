efa590aa736bd4cf047a9a667143a339b0713bbd
seohee0112 <ghdtj7@naver.com>
2021-12-28 14:22:30 +0900

    [ADD] timer component 추가

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TextInput, Keyboard, Alert } from 'react-native';
 
-const Timer = ({ mm, ss }) => {
-    const [minutes, setMinutes] = useState(parseInt(mm));
-    const [seconds, setSeconds] = useState(parseInt(ss));
+const padNumber = (num, length) => {
+    return String(num).padStart(length, '0');
+};
+
+const Timer = () => {
+    // 아무것도 입력하지 않으면 undefined가 들어오기 때문에 유효성 검사부터..
+    const tempMin = 3;
+    const tempSec = 0;
+    // 타이머를 초단위로 변환한 initialTime과 setInterval을 저장할 interval ref
+    const initialTime = useRef(tempMin * 60 + tempSec);
+    const interval = useRef(null);
+
+    const [min, setMin] = useState(padNumber(tempMin, 2));
+    const [sec, setSec] = useState(padNumber(tempSec, 2));
+
+    useEffect(() => {
+        interval.current = setInterval(() => {
+            initialTime.current -= 1;
+            setSec(padNumber(initialTime.current % 60, 2));
+            setMin(padNumber(parseInt(initialTime.current / 60), 2));
+        }, 1000);
+        return () => clearInterval(interval.current);
+    }, []);
+
+    useEffect(() => {
+        if (initialTime.current <= 0) {
+            clearInterval(interval.current);
+        }
+    }, [sec]);
 
     return (
-        <div>
-            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-        </div>
+        <View>
+            <Text>{min} : {sec}</Text>
+        </View>
     );
 };
 
--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -4,6 +4,8 @@ export const LOGOUT = "LOGOUT";
 
 export const POST_MESSAGE = "POST_MESSAGE";
 export const GET_MESSAGE = "GET_MESSAGE";
+
+
 export const REGISTER_ID_CARD = "REGISTER_ID_CARD";
 export const REGISTER_VACCINE_PASS = "REGISTER_VACCINE_PASS";
 export const REGISTER_PASSWORD = "REGISTER_PASSWORD";
