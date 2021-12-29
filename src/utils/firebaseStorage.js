42440cb8b6c3291601357aa5edc2ee757f1e7ce5
seohee0112 <ghdtj7@naver.com>
2021-12-29 22:27:30 +0900

    [ADD] 업로드할 이미지명 불러오는 함수 추가

--- src/utils/firebaseStorage.js
+++ src/utils/firebaseStorage.js
@@ -1,16 +1,16 @@
 import storage from '@react-native-firebase/storage';
+export function getRefImageName(imageName) {
+    const currentImageName = imageName.split('/');
 
+    return currentImageName[currentImageName.length - 1];
+}
 export function registerImage(imageName) {
 
-
-    let currentImageName = imageName.split('/');
-    let refImageName = currentImageName[currentImageName.length - 1];
+    let refImageName = getRefImageName(imageName);
     let reference = storage().ref(refImageName);
     let task = reference.putFile(imageName);
     console.log("success")
-    console.log(currentImageName + " / " + reference);
-    console.log(currentImageName.length);
-    console.log(refImageName);
+
     task.then(() => {
         console.log("Upload");
     }).catch((e) => {
