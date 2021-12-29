19355fd37afd0009ca57e5807b3c1c895b987365
seohee0112 <ghdtj7@naver.com>
2021-12-29 09:52:03 +0900

    [ADD] 이름 정규식 검사 함수 추가

--- src/utils/regexs.js
+++ src/utils/regexs.js
@@ -2,3 +2,8 @@ export function isPhoneNumber(phoneNumber) {
     const regex = new RegExp('^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$');
     return regex.test(phoneNumber);
 }
+
+export function isName(name) {
+    const regex = new RegExp('^[가-힣]{2,4}$');
+    return regex.test(name);
+}
