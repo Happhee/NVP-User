45c69e022a9756ae6a986b8a691ea00ec662e15f
seohee0112 <ghdtj7@naver.com>
2021-12-29 16:45:20 +0900

    [ADD] 이미지 갤러리에서 가져오는 기능 추가

--- src/components/auth/TakeVaccinePass.js
+++ src/components/auth/TakeVaccinePass.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import deviceInfoModule from 'react-native-device-info';
-import { View, Text, TextInput, Keyboard } from 'react-native';
+import { View, Text, TextInput, Keyboard, Image } from 'react-native';
 import RegisterButton from '../RegisterButton';
 import NextButton from '../NextButton';
 import signUp from '../../assets/styles/signUp';
 import { TouchableWithoutFeedback } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker';
 
-const openPicker = () => {
-    launchImageLibrary({}, (response) => {
-
-        console.log(response.assets[0].uri);
-
-    })
-
-
-}
 function TakeVaccinePass(props) {
 
+    let [imageUri, setImageUri] = useState('');
+    const openPicker = () => {
+        launchImageLibrary({}, (response) => {
+
+            console.log(response.assets[0].uri);
+            setImageUri(response.assets[0].uri);
+        })
+    }
     return (
 
         <View style={signUp.container}>
@@ -31,7 +30,8 @@ function TakeVaccinePass(props) {
                     onPress={openPicker} />
             </View>
             <View style={signUp.cameraView}>
-
+                <Image
+                    source={{ uri: imageUri }} style={{ width: '100%', height: '100%' }} />
             </View>
             <View style={signUp.footer}>
                 <NextButton
