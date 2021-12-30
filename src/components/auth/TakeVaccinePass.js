ca956252d60835ed31766c4112442a190d8a0ac5
seohee0112 <ghdtj7@naver.com>
2021-12-30 10:11:42 +0900

    [ADD] 파이어베이스 업로드 후 이미지 ocr분석할수있게 변경

--- src/components/auth/TakeVaccinePass.js
+++ src/components/auth/TakeVaccinePass.js
@@ -6,24 +6,65 @@ import NextButton from '../NextButton';
 import signUp from '../../assets/styles/signUp';
 import { TouchableWithoutFeedback } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker';
-import { registerImage, getRefImageName } from '../../utils/firebaseStorage';
-import { callGoogleVisionApi } from '../../utils/googleVision';
+
+import storage from '@react-native-firebase/storage';
+import config from '../../../config.json';
+
 function TakeVaccinePass(props) {
 
     let [imageUri, setImageUri] = useState('');
+    let [description, setDescription] = useState('');
     const openPicker = () => {
         launchImageLibrary({}, (response) => {
 
-            console.log(response.assets[0].uri);
             setImageUri(response.assets[0].uri);
 
-            console.log("가져오기 " + response.assets[0].uri);
-            // registerImage(response.assets[0].uri);
-            // const name = getRefImageName(response.assets[0].uri);
-            // console.log("이름" + name);
-            callGoogleVisionApi("gs://user-nvp.appspot.com/" + "EFA18E5C-6F75-4A8B-906E-010DD0EC8B6D.png");
+            let refImageName = getRefImageName(response.assets[0].uri);
+            let reference = storage().ref(refImageName);
+            let task = reference.putFile(response.assets[0].uri);
+            console.log("success")
+
+            task.then(() => {
+                callGoogleVisionApi("gs://user-nvp.appspot.com/" + refImageName);
+
+            }).catch((e) => {
+                console.log(e);
+            });
+
         })
     }
+
+    const callGoogleVisionApi = async (uri) => {
+        let googleVisionRes = await fetch(config.googleCloud.api + config.googleCloud.apiKey, {
+            method: 'POST',
+            body: JSON.stringify({
+                "requests": [
+                    {
+                        "features": [
+                            {
+                                "type": "DOCUMENT_TEXT_DETECTION"
+                            }
+                        ],
+                        "image": {
+                            "source": {
+                                "imageUri": uri
+                            }
+                        }
+                    }
+                ]
+            })
+        });
+
+        await googleVisionRes.text()
+            .then(googleVisionRes => {
+                console.log(googleVisionRes);
+                if (googleVisionRes) {
+                    setDescription(JSON.parse(googleVisionRes).responses[0].fullTextAnnotation.text.split('\n'));
+                }
+            }).catch((err) => { console.log(err) })
+    }
+    console.log(description)
+
     return (
 
         <View style={signUp.container}>
@@ -41,6 +82,7 @@ function TakeVaccinePass(props) {
                     source={{ uri: imageUri }} style={{ width: '100%', height: '100%' }} />
             </View>
             <View style={signUp.footer}>
+                <Text>{description}</Text>
                 <NextButton
                     onPress={function () {
                         props.navigation.navigate('CheckCertificate')
