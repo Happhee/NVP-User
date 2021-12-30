c1c9edb9a6d276991f4bd36389cad90c8043f101
seohee0112 <ghdtj7@naver.com>
2021-12-30 15:14:25 +0900

    [ADD] 신분증 이름, 파일 경로 가져오는 함수 연결

--- src/components/auth/TakeIdCard.js
+++ src/components/auth/TakeIdCard.js
@@ -4,11 +4,12 @@ import { View, Text, TextInput, Keyboard } from 'react-native';
 import RegisterButton from '../RegisterButton';
 import NvpButton from '../NvpButton';
 import signUp from '../../assets/styles/signUp';
+import ocrStyles from '../../assets/styles/ocrStyles';
 import { TouchableWithoutFeedback } from 'react-native';
 import RectangleCamera from '../RectangleCamera';
 
 function TakeIdCard(props) {
-
+    console.log(props.auth)
     return (
 
         <View style={signUp.container}>
@@ -21,7 +22,13 @@ function TakeIdCard(props) {
             </View>
 
             <View style={signUp.cameraView} >
-                <RectangleCamera />
+                <RectangleCamera setIdCard={(idCardName, idCardFilePath) => {
+
+                    props.setIdCard(idCardName, idCardFilePath)
+                    props.navigation.navigate('CheckCertificate')
+
+                }
+                } />
             </View>
 
         </View >
