f57c497787ad1bc263289762dabf712ae9c4c08e
seohee0112 <ghdtj7@naver.com>
2021-12-30 12:11:58 +0900

    [RENAME] 버튼 이름 변경

--- src/components/NvpButton.js
+++ src/components/NvpButton.js
@@ -2,16 +2,15 @@ import * as React from 'react';
 import { Button } from 'react-native-paper';
 import buttonStyles from '../assets/styles/buttonStyles';
 import Icon from 'react-native-vector-icons/FontAwesome'
-const NextButton = (props) => (
+const NvpButton = (props) => (
     <Button
         style={buttonStyles.button}
-        icon={props.icon}
         mode="contained"
         color="#00B990"
         onPress={props.onPress}>
         {props.buttonName}
-        <Icon name="arrow-right" size={60} />
+        <Icon name={props.icon} size={60} />
     </Button>
 )
 
-export default NextButton;
+export default NvpButton;
