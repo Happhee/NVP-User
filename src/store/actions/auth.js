c69cbf3787a3d3464e074a8faf0fec423f70e393
seohee0112 <ghdtj7@naver.com>
2022-01-04 08:27:02 +0900

    [REMOVE] 로그인 액션은 login에서

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,9 +2,3 @@ import * as types from './actionTypes';
 
 const USERS_URL = "/users";
 
-export function login(dataToSubmit) {
-    return {
-        type: types.LOGIN,
-        dataToSubmit
-    }
-}
