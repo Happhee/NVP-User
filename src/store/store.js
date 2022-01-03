d8c788a8a1ffc0e665caec7c2b57ac385ff6deda
seohee0112 <ghdtj7@naver.com>
2022-01-03 14:33:29 +0900

    [ADD] 미들웨어 적용

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./reducers/rootReducers";
 import thunk from 'redux-thunk'
+import loggerMiddleware from "../lib/loggerMiddleware";
 
-const store = createStore(rootReducers, applyMiddleware(thunk));
+const store = createStore(rootReducers, applyMiddleware(loggerMiddleware));
 
 export default store;
