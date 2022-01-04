ac43ad1134fd516cc5d232e4cd101fe933e641b5
seohee0112 <ghdtj7@naver.com>
2022-01-04 14:14:53 +0900

    [CONNECT] certificateReducer 루트에 연결

--- src/store/reducers/rootReducers.js
+++ src/store/reducers/rootReducers.js
@@ -3,11 +3,13 @@ import loginReducer from "./login";
 import authReducer from "./auth";
 import userReducer from "./user";
 import smsReducer from "./sms";
+import certificateReducer from "./certificate";
 const rootReducers = combineReducers({
     login: loginReducer,
     auth: authReducer,
     user: userReducer,
-    sms: smsReducer
+    sms: smsReducer,
+    certificate: certificateReducer
 });
 
 export default rootReducers;
