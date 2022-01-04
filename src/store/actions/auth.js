c1eb8bcfca4c5f9eee82897f657b88a1ef4a8e47
seohee0112 <ghdtj7@naver.com>
2022-01-04 09:32:46 +0900

    [ADD] 문자인증 성공 함수 verifySmsMessage 추가

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,51 @@
-import * as types from './actionTypes';
+import { REGISTER_ID_CARD, REGISTER_VACCINE_PASS, SET_PASSWORD, SIGN_UP, VERIFICATION_SMS_MESSAGE } from "./actionTypes";
+
 
 const USERS_URL = "/users";
 
+
+//인증 성공
+export function verifySmsMessage(id, name, phone) {
+    return {
+        type: VERIFICATION_SMS_MESSAGE,
+        id: id,
+        name: name,
+        phone: phone
+    }
+}
+
+export function setPassword(passWord) {
+
+    return {
+        type: SET_PASSWORD,
+        passWord: passWord
+    }
+}
+
+export function setVaccinePass(vaccinePassName, vaccinePassFilePath, fileName) {
+    return {
+        type: REGISTER_VACCINE_PASS,
+        vaccinePassName: vaccinePassName,
+        vaccinePassFilePath: vaccinePassFilePath,
+        fileName: fileName
+    }
+}
+
+export function setIdCard(idCardName, idCardFilePath) {
+    return {
+        type: REGISTER_ID_CARD,
+        idCardName: idCardName,
+        idCardFilePath: idCardFilePath
+    }
+}
+
+
+export function signup(dataToSubmit) {
+    // const data = request("POST", USERS_URL + '/signup', dataToSubmit);
+
+    return {
+        type: SIGN_UP,
+        payload: data,
+        data: dataToSubmit
+    }
+}
