3299f8fbc014184d40421d4e1492c0bff82fcfb4
seohee0112 <ghdtj7@naver.com>
2022-01-04 09:33:22 +0900

    [CREATE] sms 인증 reducer 및 action 생성

---
+++ src/store/actions/sms.js
@@ -0,0 +1,44 @@
+import axiosInstance from '../../lib/axiosInstance';
+import { EXPIRE_SMS_MESSAGE, SMS_MESSAGE, SMS_MESSAGE_FAILURE, SMS_MESSAGE_SUCCESS } from './actionTypes';
+
+const SMS_URL = '/sms/check';
+
+export const postMessage = (dataToSubmit) => {
+    return (dispatch) => {
+        console.log('메세지 요청');
+        dispatch(smsMessageRequest());
+        axiosInstance.get(SMS_URL, dataToSubmit)
+            .then((res) => {
+                dispatch(smsMessageSuccess(dataToSubmit.phone, res));
+            })
+            .catch((err) => {
+                dispatch(smsMessageFailure(err));
+            })
+    }
+}
+
+export const smsMessageRequest = () => {
+    return {
+        type: SMS_MESSAGE
+    };
+}
+
+export const smsMessageSuccess = (phone, message) => {
+    return {
+        type: SMS_MESSAGE_SUCCESS,
+        phone: phone,
+        payload: message
+    };
+}
+export const smsMessageFailure = (err) => {
+    return {
+        type: SMS_MESSAGE_FAILURE,
+        err: err
+    };
+}
+
+export const expireSmsMessage = () => {
+    return {
+        type: EXPIRE_SMS_MESSAGE
+    };
+}
