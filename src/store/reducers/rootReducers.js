7152dc036b994764b9087b04b47596f8e0bf015b
seohee0112 <ghdtj7@naver.com>
2022-01-04 09:33:44 +0900

    [CONNECT] sms리듀서 연결

--- src/store/reducers/rootReducers.js
+++ src/store/reducers/rootReducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import loginReducer from "./login";
 import authReducer from "./auth";
 import userReducer from "./user";
+import smsReducer from "./sms";
 const rootReducers = combineReducers({
     login: loginReducer,
     auth: authReducer,
     user: userReducer,
+    sms: smsReducer
 });
 
 export default rootReducers;
