a6a9a03dc720413da426ab349fe35cef78364825
seohee0112 <ghdtj7@naver.com>
2022-01-05 16:31:31 +0900

    [CONNECT] 로그아웃 연결

--- src/components/Setting/Logout.js
+++ src/components/Setting/Logout.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Keyboard, Alert } from 'react-native';
-
 import authStyles from '../../assets/styles/authStyles';
+import settingStyles from '../../assets/styles/setting';
+import UserButton from '../UserButton';
 
-function Logout(props) {
+function Logout({ props, navigation }) {
 
+    console.log(navigation.popToTop())
     return (
-
-        <View style={authStyles.container}>
-            <View style={authStyles.header}>
-                <Text style={authStyles.headerFont}>로그아웃</Text>
+        <View style={settingStyles.container}>
+            <View style={settingStyles.settingView}>
+                <Text style={settingStyles.contentFont}>정말로 로그아웃을 하시겠습니까?</Text>
+                <View style={settingStyles.buttonView}>
+                    <UserButton buttonName="로그아웃 하기"
+                        onPress={function () {
+                            // props.logout();
+                            // props.navigation.navigate('Login');
+                        }}
+                    />
+                </View>
             </View>
-
         </View>
     )
 
