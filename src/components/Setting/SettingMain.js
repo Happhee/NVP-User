ea15727bdb8927194319fe0f3cb85f4690b3ea8f
seohee0112 <ghdtj7@naver.com>
2022-01-05 10:22:59 +0900

    [ADD] 설정 메인 화면 ui설정

--- src/components/Setting/SettingMain.js
+++ src/components/Setting/SettingMain.js
@@ -1,15 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Keyboard, Alert } from 'react-native';
-
-import authStyles from '../../assets/styles/authStyles';
+import RegisterButton from '../RegisterButton';
+import settingStyles from '../../assets/styles/setting';
 
 function SettingMain(props) {
-
     return (
 
-        <View style={authStyles.container}>
-            <View style={authStyles.header}>
-                <Text style={authStyles.headerFont}>설정 메인</Text>
+        <View style={settingStyles.container}>
+            <View style={settingStyles.header}>
+                <RegisterButton buttonName="백신패스 등록" />
+            </View>
+
+            <View style={settingStyles.content}
+                onTouchStart={() => props.navigation.navigate('ConfirmVaccinePass')} >
+                <Text style={settingStyles.contentFont} >백신패스 확인</Text>
+            </View>
+
+
+
+            <View style={settingStyles.content}
+                onTouchStart={() => props.navigation.navigate('ConfirmVaccinePass')} >
+
+                <Text style={settingStyles.contentFont}>비밀번호 설정</Text>
+
+            </View>
+            <View style={settingStyles.content}
+                onTouchStart={() => props.navigation.navigate('Logout')} >
+
+                <Text style={settingStyles.contentFont}>로그아웃</Text>
+
+            </View>
+            <View style={settingStyles.content}
+                onTouchStart={() => props.navigation.navigate('WithdrawalUser')} >
+
+                <Text style={settingStyles.contentFont}>회원탈퇴</Text>
+
             </View>
 
         </View>
