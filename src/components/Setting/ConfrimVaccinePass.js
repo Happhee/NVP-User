1f1187720b6b5cd5cec7dff048080aa5ba8ab525
seohee0112 <ghdtj7@naver.com>
2022-01-06 09:15:55 +0900

    [CONNECT] 백신패스 확인 연결

--- src/components/Setting/ConfrimVaccinePass.js
+++ src/components/Setting/ConfrimVaccinePass.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Keyboard, Alert } from 'react-native';
+import { View, Text, TextInput, Keyboard, Alert, Button } from 'react-native';
 
 import authStyles from '../../assets/styles/authStyles';
 
 function ConfrimVaccinePass(props) {
+
 
     return (
 
         <View style={authStyles.container}>
             <View style={authStyles.header}>
                 <Text style={authStyles.headerFont}>백신패스 확인</Text>
+                <Button onPress={() => {
+                    props.downLoadCertificate({ filename: 'redux.png' });
+
+                }} title="가져오기" />
             </View>
 
         </View>
--- src/containers/Setting/ConfirmVaccinePass.js
+++ src/containers/Setting/ConfirmVaccinePass.js
@@ -1,13 +1,15 @@
 import ConfrimVaccinePass from "../../components/Setting/ConfrimVaccinePass";
 import { connect } from "react-redux";
-
+import { downLoadCertificate } from "../../store/actions/certificate";
 function mapReduxStateToReactProps(state) {
     return state;
 }
 
 function mapReduxDispatchToReactProps(dispatch) {
     return {
-
+        downLoadCertificate: function (dataToSubmit) {
+            dispatch(downLoadCertificate(dataToSubmit))
+        }
     }
 }
 
