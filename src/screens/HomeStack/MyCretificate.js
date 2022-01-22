2b681d12171976f56b9ff77da33ab2ee3ffe18d2
jihoon98 <tlswlgns1003@sju.ac.kr>
2022-01-22 23:10:06 +0900

    확인팝업창 만들기

--- src/screens/HomeStack/MyCretificate.js
+++ src/screens/HomeStack/MyCretificate.js
@@ -1,32 +1,117 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+import React,{Component} from 'react';
+import {Alert, Modal, View, Text, TouchableOpacity, StyleSheet,Pressable,Dimensions } from 'react-native';
+import NfcManager, {NfcTech, ByteParser, Ndef} from 'react-native-nfc-manager';
 
-NfcManager.start();
 
-function App() {
-  async function readNdef() {
-    try {
-      await NfcManager.requestTechnology(NfcTech.Ndef);
-      const tag = await NfcManager.getTag();
-      console.warn('Tag found', tag);
-    } catch (ex) {
-      console.warn('Oops!', ex);
-    } finally {
-      NfcManager.cancelTechnologyRequest();
+class Cre extends Component{
+    
+    state = {
+        modalVisible: false
+    };
+    constructor(props){
+        super(props);
+        this.readNdef();
+        
     }
-  }
+    setModalVisible = (visible) => {
+        this.setState({ modalVisible: visible });
+      }
+    componentDidMount(){
+        NfcManager.start();
+    }
+    readNdef=async()=>{
+        await NfcManager.requestTechnology(NfcTech.Ndef);
+        const tag = await NfcManager.getTag();
+        try {
+            console.warn('태그 정보', tag);
+            
+          } catch (ex) 
+          {
+            console.warn('Oops!', ex);
+          } finally
+           {
+            NfcManager.cancelTechnologyRequest();
+          }
+    }
+    render(){
 
-  return (
-    <View style={styles.wrapper}>
-      <TouchableOpacity onPress={readNdef} style={styles.button}>
-      <Text>NVP</Text>
-      </TouchableOpacity>
-    </View>
-  );
+        const { modalVisible } = this.state;
+        return (
+            <View style={styles.centeredView}>
+              <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("팝업창이 닫혔습니다");
+            this.setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>인증되었습니다!</Text>
+              <Text onClick={this.log}>
+=              </Text>
+              <Pressable
+                onPress={() => this.setModalVisible(!modalVisible)}>
+                <Text>닫기</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <Pressable
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => this.setModalVisible(true)}>
+          <Text style={styles.textStyle}>NVP</Text>
+        </Pressable>
+        </View>
+          )
+    }
 }
-
 const styles = StyleSheet.create({
+
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+      },
+      buttonOpen: {
+        backgroundColor: "#00B990",
+      },
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: Dimensions.get('window').width > 500? 36 : 30
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+      },
   wrapper: {
     flex: 1,
     justifyContent: 'center',
@@ -62,4 +147,4 @@ text: {
 }
 });
 
-export default App;
+export default Cre;
